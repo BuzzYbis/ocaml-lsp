9d5930a2d7c79dbddcb1e85402ebc4d017a53f47
ugo <ugo.gosso@ecole2600.com>
2026-03-23 00:15:55 +0100

    Try fixing error String.is_suffix
    Fix: Fix: Add `is_suffix = is_suffix` inside import.ml

--- ocaml-lsp-server/src/import.ml
+++ ocaml-lsp-server/src/import.ml
@@ -79,6 +79,7 @@ module String = struct
     let substr_index = substr_index
     let prefix = prefix
     let is_prefix = is_prefix
+    let is_suffix = is_suffix
     let lfindi = lfindi
     let filter = filter
   end
